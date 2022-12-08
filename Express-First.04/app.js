874b05daf53374d5d61b3c4fe9216d345e7c5897
George Kakarlis <georgekakarlis@yahoo.com>
2022-12-08 15:42:17 +0100

    parsing requests and limiting middleware execution to POST request

--- Express-First.04/app.js
+++ Express-First.04/app.js
@@ -1,10 +1,11 @@
 //general note : always travelling top-to-bottom and the middlewares always work like that
 
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const app = express(); //app does a lot in the background. check with command + click for the source code written in defintion typescript (*.d.ts)
 
-
+app.use(bodyParser.urlencoded({extended: false}));
 //commented out to proceed learning
 /* app.use((req, res, next) => {
     console.log('midleware')
@@ -14,9 +15,15 @@ const app = express(); //app does a lot in the background. check with command + 
 
 // duplicated for the sake of next()
 app.use('/add-product', (req, res, next) => {
-    console.log('midleware2')
-    res.send('<h1>add product page</h1>'); //res.send() and send in general also .send() sets headers in html automatically
-})
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  });
+
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 
 app.use('/', (req, res, next) => {
     console.log('midleware2')
