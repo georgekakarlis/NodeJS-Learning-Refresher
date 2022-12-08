e5aa96f275837aee16dccf04bdf2bdeb3ed4b9fd
George Kakarlis <georgekakarlis@yahoo.com>
2022-12-08 15:26:44 +0100

    added some routing/next() clarified better

--- Express-First.04/app.js
+++ Express-First.04/app.js
@@ -1,19 +1,26 @@
 //general note : always travelling top-to-bottom and the middlewares always work like that
-const http = require('http')
+
 const express = require('express')
 
 const app = express(); //app does a lot in the background. check with command + click for the source code written in defintion typescript (*.d.ts)
 
-app.use((req, res, next) => {
+
+//commented out to proceed learning
+/* app.use((req, res, next) => {
     console.log('midleware')
     next(); // allows the request to continue to the next middleware in line
+}) */
+
+
+// duplicated for the sake of next()
+app.use('/add-product', (req, res, next) => {
+    console.log('midleware2')
+    res.send('<h1>add product page</h1>'); //res.send() and send in general also .send() sets headers in html automatically
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('midleware2')
     res.send('<h1>hello froms express</h1>'); //res.send() and send in general also .send() sets headers in html automatically
 })
 
-const server = http.createServer(app)
-
-server.listen(3000)
+app.listen(3000); //this is less code but same functionality. because of express.js this is done in smaller chunks
